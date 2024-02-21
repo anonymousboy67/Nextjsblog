9262c1e3e9812a3e5ea42a4895347cec7d33257a
anonymousboy67 <aashishad67@gmail.com>
2024-02-21 10:10:27 +0545

    Navbar with routes

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,35 @@
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
+
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
+const Links = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Links;
