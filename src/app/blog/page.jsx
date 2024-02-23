d374a782376a8e577b9b697283a5318c4d20a97d
anonymousboy67 <aashishad67@gmail.com>
2024-02-23 09:44:46 +0545

    PostCard container

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,13 @@
+import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
+
+
+const BlogPage=()=>{
+    return<div className={styles.container}>
+        <PostCard/>
+        
+
+    </div>
+}
+
+export default BlogPage;
