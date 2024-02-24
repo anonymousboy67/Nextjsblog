d6641255a51c18f885a77c1554a62bfe95af237a
anonymousboy67 <aashishad67@gmail.com>
2024-02-24 14:36:31 +0545

    Creating middleware

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,23 +1,19 @@
-import {auth, signIn} from "@/lib/auth";
+import LoginForm from "@/components/loginForm/loginForm";
+import { handleGithubLogin } from "@/lib/action";
+import styles from "./login.module.css";
 
-const LoginPage=async()=>{
+const LoginPage = () => {
 
-    const session=await auth();
-
-
-    const handleGithubLogin=async()=>{
-        "use server";
-        await signIn("github");
-
-    };
-
-    return(
-        <div>
-            <form action={handleGithubLogin}>
-                <button>Login with Github</button>
-            </form>
-        </div>
-    );
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form action={handleGithubLogin}>
+          <button className={styles.github}>Login with Github</button>
+        </form>
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
